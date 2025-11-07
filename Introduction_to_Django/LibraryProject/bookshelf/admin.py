c100d93107b6737e2952b74a326e20a15ef295e8
Truket Lema <truketlema9489@gmail.com>
2025-11-07 03:19:08 +0300

    Fix admin.py to include ModelAdmin, list_display, search_fields, list_filter

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Book
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  
+    search_fields = ('title', 'author')                     
+    list_filter = ('publication_year',)                     
+
+admin.site.register(Book, BookAdmin)
